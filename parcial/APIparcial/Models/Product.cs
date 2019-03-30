f1f2c5ce15b8634f4c87c518450df0fe89dda270
Matias Derek Torrico Morgan <matias_dtm@hotmail.com>
2019-03-30 11:18:46 -0400

    Commit proyecto Api

---
+++ parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIparcial.Models
+{
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public TipoUnidad Unidad { get; set; }
+        [Required]
+        public TipoEstado Estado { get; set; }
+    }
+}
