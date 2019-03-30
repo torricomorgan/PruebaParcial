b747173bae13c7f09296e53985bb4032945179c7
Matias Derek Torrico Morgan <matias_dtm@hotmail.com>
2019-03-30 11:00:40 -0400

    Commit modelo MVC

---
+++ parcial/MVCparcial/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCparcial.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCparcial.Models.Product> Products { get; set; }
+    }
+}
